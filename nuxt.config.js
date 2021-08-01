fd03b5a3793858b6678de4a1af6fc2877de61310
DerLev <levin.schroeren@gmail.com>
2021-08-01 19:49:48 +0200

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,8 +18,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'application-name', content: 'McMineserver' },
-      { name: 'msapplication-TileImage', content: 'favicon/favicon-144.png' },
+      { name: 'application-name', content: 'McMineserver SLF' },
+      { name: 'msapplication-TileImage', content: '/favicon/favicon-144.png' },
       { name: 'msapplication-TileColor', content: '#121417' }
     ],
     link: [
